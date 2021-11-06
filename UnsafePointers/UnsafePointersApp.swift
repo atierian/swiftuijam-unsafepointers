cf2024321094589181e1c0c40ff9c25c1380d545
Ian Saultz <52051793+atierian@users.noreply.github.com>
2021-11-06 00:54:40 -0400

    Add comment header layout and remove generated example code

--- UnsafePointers/UnsafePointersApp.swift
+++ UnsafePointers/UnsafePointersApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct UnsafePointersApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+          ReaderView()
         }
     }
 }
---
+++ UnsafePointers/Views/MenuView.swift
@@ -0,0 +1,9 @@
+//
+// MenuView.swift
+// UnsafePointers
+//
+// Created by UnsafePointers on 11/6/21.
+// License: Apache 2.0
+  
+
+import Foundation
