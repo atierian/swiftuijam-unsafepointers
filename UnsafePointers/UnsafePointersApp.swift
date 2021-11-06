eaf1fb6bcbff4b0ff363bde9f04266c7dc0efcfb
Ian Saultz <52051793+atierian@users.noreply.github.com>
2021-11-05 23:45:38 -0400

    Initial Commit

---
+++ UnsafePointers/UnsafePointersApp.swift
@@ -0,0 +1,20 @@
+//
+//  UnsafePointersApp.swift
+//  UnsafePointers
+//
+//  Created by alphatier on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct UnsafePointersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
